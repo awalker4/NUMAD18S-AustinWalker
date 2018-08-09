ab1e299ba95cf633f479a4d3e4621e4ab73605ec
Austin Walker <awalk89@gmail.com>
2018-08-09 00:45:12 -0400

    Remove import

--- app/src/main/java/edu/neu/madcourse/austinwalker/treble/LevelSelectTile.java
+++ app/src/main/java/edu/neu/madcourse/austinwalker/treble/LevelSelectTile.java
@@ -1,7 +1,6 @@
 package edu.neu.madcourse.austinwalker.treble;
 
 import android.graphics.drawable.Drawable;
-import android.text.BoringLayout;
 import android.view.View;
 import android.widget.Button;
 
